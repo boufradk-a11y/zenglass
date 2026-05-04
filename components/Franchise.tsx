6405a149e0c71fcc79fb8b0679ff7c8bbe8af549
MadebyChecone <ryancecon7@gmail.com>
2026-05-04 18:03:12 +0200

    test

--- components/Franchise.tsx
+++ components/Franchise.tsx
@@ -55,6 +55,7 @@ export function Franchise() {
                   </MotionDiv>
                 ))}
               </div>
+              
 
               <div className="flex flex-col sm:flex-row gap-4">
                 <a 
